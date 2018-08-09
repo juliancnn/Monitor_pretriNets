54da702f08db2c73cbbb9866fd6390db0a427797
Julian Morales <jmorales@unc.edu.ar>
2018-08-09 19:55:40 -0300

    fix

--- source/tpc/monitor/RDP.java
+++ source/tpc/monitor/RDP.java
@@ -1,24 +1,18 @@
 package tpc.monitor;
-
 import java.lang.String;
-import java.lang.reflect.Array;
-import java.util.Vector;
+
 
 public class RDP {
 
-  private int[][] mRDP;
-  /**
-   * @brief Matriz de la red de petri
-   */
+  private int[][] mRDP; /** @brief Matriz de la red de petri  */
   private int[] mark; /** @brief Marcador de la red de petri */
 
   /**
    * @param fileMatrix Nombre del archivo
-   *                   <p>
-   *                   <p>
-   *                   El constructor setea el marcador inicial en cero para todas las plazas
-   *                   Las filas estan separadas por saltos de linea y las columnas por espacios
    * @brief Crea la red de petri a partir de un archivo
+   *
+   * El constructor setea el marcador inicial en cero para todas las plazas
+   * Las filas estan separadas por saltos de linea y las columnas por espacios
    * @TODO emplentar
    */
   public RDP(String fileMatrix) {
