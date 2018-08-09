c2ab70be76e80892b59098d66cf51725374b7931
Julian Morales <jmorales@unc.edu.ar>
2018-08-09 19:02:20 -0300

    RDP Struct

--- source/Main.java
+++ source/Main.java
@@ -1,3 +1,5 @@
+import tpc.monitor.RDP;
+
 public class Main {
 
     public static void main(String[] args) {
--- source/tpc/monitor/RDP.java
+++ source/tpc/monitor/RDP.java
@@ -1,4 +1,32 @@
 package tpc.monitor;
+import java.lang.String;
+import java.lang.reflect.Array;
 
 public class RDP {
+
+    /**
+     * @brief Crea la red de petri a partir de un archivo
+     * @param fileMatrix Nombre del archivo
+     *
+     *
+     * El constructor setea el marcador inicial en cero para todas las plazas
+     * Las filas estan separadas por saltos de linea y las columnas por espacios
+     * @TODO emplentar
+     * */
+    public RDP(String fileMatrix){
+
+    }
+
+    /**
+     * @brief Setea el marcador en base al array recibido
+     * @param nMark Array del markador
+     * @TODO leerlo desde un archivo
+     * @TODO Implementar las exepciones al leer el archivo
+     * */
+    public boolean setMarkStatus(int[] nMark){
+
+        return false;
+    }
+
+
 }
