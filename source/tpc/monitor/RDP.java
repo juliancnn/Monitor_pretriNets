2842f542506be4b7aae299e5217195286597c261
MnLujan <martinlujan96@gmail.com>
2018-08-09 19:52:54 -0300

    basic struc

--- source/tpc/monitor/RDP.java
+++ source/tpc/monitor/RDP.java
@@ -1,32 +1,51 @@
 package tpc.monitor;
+
 import java.lang.String;
 import java.lang.reflect.Array;
+import java.util.Vector;
 
 public class RDP {
 
-    /**
-     * @brief Crea la red de petri a partir de un archivo
-     * @param fileMatrix Nombre del archivo
-     *
-     *
-     * El constructor setea el marcador inicial en cero para todas las plazas
-     * Las filas estan separadas por saltos de linea y las columnas por espacios
-     * @TODO emplentar
-     * */
-    public RDP(String fileMatrix){
+  private int[][] mRDP;
+  /**
+   * @brief Matriz de la red de petri
+   */
+  private int[] mark; /** @brief Marcador de la red de petri */
 
-    }
+  /**
+   * @param fileMatrix Nombre del archivo
+   *                   <p>
+   *                   <p>
+   *                   El constructor setea el marcador inicial en cero para todas las plazas
+   *                   Las filas estan separadas por saltos de linea y las columnas por espacios
+   * @brief Crea la red de petri a partir de un archivo
+   * @TODO emplentar
+   */
+  public RDP(String fileMatrix) {
 
-    /**
-     * @brief Setea el marcador en base al array recibido
-     * @param nMark Array del markador
-     * @TODO leerlo desde un archivo
-     * @TODO Implementar las exepciones al leer el archivo
-     * */
-    public boolean setMarkStatus(int[] nMark){
+    mRDP = new int[][]
+            {
+                    {-1, 0, 0, 1},
+                    {1, -1, 0, 0},
+                    {0, 1, 0, -1},
+                    {1, 0, 0, -1},
+                    {0, 0, 1, -1}
 
-        return false;
-    }
+            };
+  }
+
+  /**
+   * @param nMark Array del markador
+   * @brief Setea el marcador en base al array recibido
+   * @TODO leerlo desde un archivo
+   * @TODO Implementar las exepciones al leer el archivo
+   */
+  public boolean setMarkStatus(int[] nMark) {
+
+    mark = new int[]{1, 0, 0, 0, 0};
+
+    return false;
+  }
 
 
 }
