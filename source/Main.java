790cfc2f314b6db2791508e86cfa95f22692ff56
Julian Morales <jmorales@unc.edu.ar>
2018-08-21 00:40:16 -0300

    Fuck the main

--- source/Main.java
+++ source/Main.java
@@ -9,7 +9,19 @@ public class Main {
         rdp.printMark();
         System.out.println("========================");
         rdp.printSensitizedVector();
+        try {
+            rdp.shotT(1, false);
+            rdp.shotT(2, false);
+            rdp.shotT(3, false);
+            rdp.shotT(0, false);
+        }catch (RDP.ShotException e){
+            e.printInfo();
+        }
 
+        System.out.println("========================");
+        rdp.printMark();
+        System.out.println("========================");
+        rdp.printSensitizedVector();
 
     }
 }
