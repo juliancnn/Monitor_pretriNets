673736ab404cea07fdc87400442ef03f0eb9c696
MnLujan <martinlujan96@gmail.com>
2018-08-26 01:10:07 -0300

    remove @TODO

--- source/tpc/monitor/RDP.java
+++ source/tpc/monitor/RDP.java
@@ -16,8 +16,6 @@ import java.util.*;
  *
  * @WARNING No implementa ningun mecanismo de proteccion de recursos para hilos multiples (como semaforo),
  * debe ser implementado externamente
- * @TODO Implementar metodo protect boolean(Puede o no) setTokens(plaza, cantidad>0): Exepcion si la plaza no existe
- * Exepcion si la cantidad es invalida (<1)
  * @TODO Implementar arcos lectores e inibidores
  * @TODO Implementar las transiciones temporales
  *
