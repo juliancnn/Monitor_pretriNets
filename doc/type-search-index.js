b4c9b7f20789b98023b374e8093f8a4461b2ed44
Julian Morales <jmorales@unc.edu.ar>
2018-09-06 20:56:47 -0300

    Remove doc from repo

--- doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"p":"tpc.monitor.rdp","l":"ConfigException"},{"p":"tpc.monitor.rdp","l":"errorTypeConfig"},{"p":"<Unnamed>","l":"Main"},{"p":"tpc.monitor.rdp","l":"rawRDP"},{"p":"tpc.monitor.rdp","l":"RDP"},{"p":"tpc.monitor.rdp","l":"RDPTest"},{"p":"tpc.monitor.rdp","l":"ShotException"},{"p":"tpc.monitor.rdp","l":"TokenException"}]
