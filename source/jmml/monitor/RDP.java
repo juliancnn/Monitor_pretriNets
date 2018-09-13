ed4572ec1eeda21f37586f533c3cebf50e724405
Julian Morales <jmorales@unc.edu.ar>
2018-09-13 18:55:46 -0300

    shot con max tokens

--- source/jmml/monitor/RDP.java
+++ source/jmml/monitor/RDP.java
@@ -17,6 +17,7 @@ import java.lang.String;
  *  - [NO] Informar si se puede disparar o no una transicion
  *  - [NO] Informar el marcado de la red
  *  - [NO] Informar el estado de sensibilizado de todas las condiciones (Matrix sensibilizado)
+ *  - [NO] Imprimir toda la informacion disponible de la red y su estado
  *
  * Soporta caracteristicas de redes de petri extendidas como:
  *  - [NO] Maxima cantidad de tokens por plaza
@@ -39,14 +40,14 @@ public class RDP {
      * Crea la red de petri a partir de un archivo
      * <p>
      * El constructor setea el marcador inicial, la rdp y su configuracion en base a un archivo json de la
-     * misma estructura de rawRDP. (Vease rawRDP para ver la estructura completa del archivo)
+     * misma estructura de RDPraw. (Vease RDPraw para ver la estructura completa del archivo)
      * <pre>
      *   Estructura del JSON:
      *
      *    {
      *     "brief" : "Un breve descripcion de la red"
      *     "info"  : "Una descripcion mas detallada de la red",
-     *     "matrixI" : [                             # La matriz de doble incidencia
+     *     "matrixI" : [                              # La matriz de doble incidencia
      *       [-1, 0, 0, 1],
      *       [1, -1, 0, 0],
      *       [0, 1, 0, -1],
@@ -61,7 +62,6 @@ public class RDP {
      * @throws FileNotFoundException Lanzado cuando no se encuentra el archivo JSON
      * @see RDPraw Ver estructura completa del JSON
      */
-
     public RDP(String jsonFile) throws FileNotFoundException {
 
         /* INICIO DE CARGA DE DATOS */
@@ -70,4 +70,102 @@ public class RDP {
         this.raw = json.fromJson(reader, RDPraw.class);
         /* FIN DE CARGA DE DATOS */
     }
+
+    // Falta agregar las escepciones si no existe la transiciones
+    public boolean shotT(int tDisp, boolean test){
+        boolean validShot = true;
+        int[] newMark;
+
+        /* Si el tiro sigue siendo valido chequeo nueva marca */
+        newMark = validShot ? this.nextMark(tDisp) : null;
+        validShot = newMark != null && valid4Mark(newMark);
+
+
+        if(validShot && !test){
+            this.raw.vectorMark = newMark;
+        }
+
+        return validShot;
+    }
+
+    /**
+     * <pre>
+     * Analiza si la marca es valida, tomando en cuenta la posibilidad de maxima cantidad de tokens
+     * @param mark Vector marca a analizar
+     * @return True si la marca es valida.
+     *         False si la marca no es valida
+     * </pre>
+     */
+    private boolean valid4Mark(int[] mark){
+        boolean valid = true;
+
+        for(int i=0;i<mark.length;i++){
+            // Marca invalida por falta de tokens
+            if(mark[i]<0){
+                valid = false;
+                break;
+            }
+            // Cambio por la funcion si es extendida
+            // Marca invalida por maxTokens
+            if(this.raw.vectorMaxMark != null){
+                if(this.raw.vectorMaxMark[i] != 0 && mark[i] > this.raw.vectorMaxMark[i]){
+                    valid = false;
+                    break;
+                }
+
+            }
+        }
+
+        return valid;
+    }
+
+    /**
+     * Retorna el posible resultado del proximo marcado luego del disparo tDisp
+     * <pre>
+     * Retorna el posible resultado del disparo sin alterar el marcador y sin verificar su validez <br>
+     * Calcula el marcado del disparo de 1 sola transicion, como: <br>
+     * Nuevo mark = mark actual + RDP * Vector Disparo
+     * @param tDisp numero de transicion a disparar
+     * @return vectorNextMark  Proxima marca, sea alcanzable o no. Null en caso de inexistencia de transicion
+     * </pre>
+     */
+    private int[] nextMark(int tDisp){
+        // Si la transicion no existe
+        if (tDisp > raw.matrixI[0].length || tDisp < 1)
+            return null;
+
+        // Vector de disparo ()
+        int[] vectorDisparo = new int[raw.matrixI[0].length];
+        vectorDisparo[tDisp - 1] = 1;
+
+        // vector Proximo marcado
+        return matMulVect(this.raw.matrixI,vectorDisparo);
+
+    }
+
+    /**
+     * Multiplica la Matriz M por el vector V
+     * @param m Matriz de dimencion MxN
+     * @param v Vector de dimencion Nx1
+     * @return vector de Nx1, NULL en caso de tamanos incompatibles
+     */
+    private int[] matMulVect(int[][] m, int[] v){
+        // Chequeo tamanos compatibles
+        if(v.length != m[0].length)
+            return null;
+
+        // Vector resultado inicializado en 0
+        int[] result = new int[v.length];
+
+        // Opero por filas en la matriz
+        for(int i = 0; i < m.length; i++){
+            for(int j=0; j<m[0].length;j++){
+                result[i] += m[i][j] * v[j];
+            }
+        }
+
+        return result;
+    }
+
+
 }
