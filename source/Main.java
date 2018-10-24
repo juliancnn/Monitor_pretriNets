b4888aa32920199cc0e06e993c432de10ffcb3cc
Julian Morales <jmorales@unc.edu.ar>
2018-10-24 14:07:02 -0300

    Nueva petri

--- source/Main.java
+++ source/Main.java
@@ -1,21 +1,8 @@
-import jmml.monitor.parser.DataParser;
-import jmml.monitor.rdp.RDPraw;
-
-import java.io.FileNotFoundException;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        try{
-            DataParser dp = new DataParser("examples_rdp/ej1_extended_Temporal.json");
-            RDPraw rdpRaw = dp.generate(RDPraw.class);
-            System.out.print(String.valueOf(rdpRaw.toString()));
-            RDPraw rdpRaw2 = dp.generate(RDPraw.class);
-            System.out.print(String.valueOf(rdpRaw.toString()));
-        }catch (FileNotFoundException e){
-            System.out.println("Archeeevo no encontrado");
-        }
 
 
 
